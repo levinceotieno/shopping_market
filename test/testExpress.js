2c5462e13b163e4da1a8f33b5bdfb27a7d250e3c
susan-kimemia <susankimemia111@gmail.com>
2024-07-17 13:51:59 +0300

    Adjusted the express.js test

--- test/testExpress.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');  // Library for making HTTP requests
-const app = require('../app');  // Path to your Express app
-
-test('GET /users returns a list of users', async () => {
-  const response = await request(app).get('/users');
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toEqual(expectedUserList); // Expected response data
-});
